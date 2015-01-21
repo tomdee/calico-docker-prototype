c6283a6dce705f5d25b6f4154692520f5f2cdb75
Tom Denham <tom@tomdee.co.uk>
2015-01-20 20:53:06 -0800

    Add log upload scrip

--- dump_logs.sh
+++ dump_logs.sh
@@ -1,3 +1,12 @@
 for i in calicodockerprototype_aclmanager_1 calicodockerprototype_bird_1 calicodockerprototype_felix_1 calicodockerprototype_pluginnetwork_1 calicodockerprototype_pluginep_1; do
-sudo docker logs $i >$i.txt 2>&1
+	sudo docker logs $i >/tmp/$i.txt 2>&1
 done
+
+FILENAME=diags-`date +%Y%m%d_%H%M%S`.tar.gz
+
+echo "Adding files to $FILENAME"
+tar -zcvf $FILENAME  /tmp/calico* config/* config/data/*
+
+echo "Uploading file. It will be available for 14 days from the following URL"
+curl -# --upload-file $FILENAME https://transfer.sh/$FILENAME
+
